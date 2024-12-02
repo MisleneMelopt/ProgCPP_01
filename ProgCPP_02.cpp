3146e5904206e806ab0638a50bf1d4f93d9e884a
MisleneMelopt <mismelo81@gmail.com>
2024-12-02 16:53:48 +0000

    Prog02
    Exercício2

---
+++ ProgCPP_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int idade;
+    float altura;
+    cout << "Introduza a sua idade:" ;
+        cin>> idade;
+    cout << "Introduza a sua altura:" ;
+        cin>> altura;
+
+cout<< "A sua idade é: " << idade << "A sua altura é: " << altura << endl;
+
+    return 0;
+
+}
