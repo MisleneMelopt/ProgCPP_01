daebabc92451b55dfa186b39acf3748a248b2791
MisleneMelopt <mismelo81@gmail.com>
2024-12-09 16:05:24 +0000

    Poo_01
    Primeiro programa

---
+++ 09/12/poo_01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+//#01: Definição da classe
+class nomeClasse{
+	//#02: Declaração de membros privados (dados e funções) da classe
+		private:
+			int valorPrivado;
+		
+	//03: Declaração de membros públicos (dados e funções)da classe 
+		public:
+			int valorPublico;
+			
+			void funcaoPublica() {
+				count << "esta é a minha primeira função" << end1;
+			}
+}; // Fim da classe 'nomeClasse? com o terminador ';'
+
+int main(){
+	// #04: Criação de um objeto do tipo da classe
+	nomeClasse objectoNovo;
+	
+	//#05: Podemos aceder e modificar os dados de um objecto público
+	objectoNovo.valorPublico = 10;
+	
+	//#06: E também podemos usar as funções definidas dentro da classe
+		objectoNovo.funcaoPublica();
+		
+		return 0;
+}
+
+
+		
+	
