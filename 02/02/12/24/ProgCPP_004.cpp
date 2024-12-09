aa09bc687e6bd21693dfda134885cf63dace3f8c
MisleneMelopt <mismelo81@gmail.com>
2024-12-09 15:01:49 +0000

    Prog3e 4

---
+++ 02/02/12/24/ProgCPP_004.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>  
+// using namespace std;  
+// int main( ) {  
+//    char ary[] = "Welcome to C++ tutorial";  
+//    cout << "Value of ary is: " << ary << endl;  
+// }  
+
+#include <iostream>  
+using namespace std;  
+int main( ) {  
+   int age;  
+   float altura;
+   cout << "Introduza a sua idade: ";  
+     cin >> age;  
+   cout << "Introduza a sua altura: ";  
+     cin >> altura;  
+   cout << "A sua idade é " << age << " e a altura é " << altura << endl;
+   return 0;  
+}  
+
