e00fcaffe530e13794c6b4db9ea2a0a5429c7357
MisleneMelopt <mismelo81@gmail.com>
2024-12-09 16:40:03 +0000

    Update poo_02.cpp

---
+++ 09/12/poo_02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Class Estudante {
+    public:
+        string nome;
+        int idade;
+
+        void mostrar(){
+            cout << nome << " " << idade << endl;
+        }
+};
+
+int main(){
+    Estudante novoEstudante;
+    cin>> novoEstudante.nome >> novoEstudante.idade;
+    novoEstudante.mostrar();
+
+    return 0;
+    
+}
